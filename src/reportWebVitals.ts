9fe63863507b920395213579b3d2907b8b87dfa9
Zahoor Ahmed <zdev1989@gmail.com>
2024-07-27 09:04:20 +0200

    Added initial changes and project setup

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,14 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onFID, onLCP, onTTFB, Metric } from 'web-vitals';
+
+type ReportHandler = (metric: Metric) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    }
 };
 
 export default reportWebVitals;
